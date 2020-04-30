cea0d0bebf8115fe993954a3b71c64bdb708b0dc
fiplox <56274824+fiplox@users.noreply.github.com>
2020-04-30 13:52:06 +0200

    fish aliases

---
+++ config.fish
@@ -0,0 +1,17 @@
+alias ls='lsd'
+alias update='sudo pacman -Syu'
+alias c='clear'
+alias search='pacman -Ss'
+alias cp="cp -i"              
+alias clean='sudo pacman -Rsn (pacman -Qdtq)'
+alias vi=vim
+alias svi='sudo vim'
+alias svim='sudo vim'
+alias mv='mv -i'
+alias ln='ln -i'
+alias rm='rm -i'
+alias untar='tar -zxvf'
+alias la='lsd -A'
+alias ll='lsd -Al'
+alias lt='lsd --tree'
+set -g theme_display_date no
