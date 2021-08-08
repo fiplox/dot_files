79e733f0cbac5ea2a9afa1d7823605f96e7e39e7
fiplox <56274824+fiplox@users.noreply.github.com>
2021-08-08 12:45:10 +0200

    rm alt backslash binding

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -106,7 +106,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_equal,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_backslash, zoom,           {0} },
+	/* { MODKEY,                       XK_backslash, zoom,           {0} }, */
 	{ MODKEY,                       XK_grave, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
