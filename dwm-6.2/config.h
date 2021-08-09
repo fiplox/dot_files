88e90c4c7e6b92c685c1558d3d5e4f42507db7ad
fiplox <56274824+fiplox@users.noreply.github.com>
2021-08-09 19:25:04 +0200

    patch to draw status on all monitors

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -34,6 +34,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	//{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
+	{ "Chromium",  NULL,       NULL,       1,       0,           0 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ NULL,  NULL,       "do_float",       1 << 8,       1,           -1 },
 };
